bce8946d1d19e8651fc4f663011be0e448c1a20b
alexvandaluz <alexvandaluz@gmail.com>
2024-02-13 18:36:08 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-let contador = 0
-do{
-    console.log("ola")
-    contador++
-}while(contador < 3)
+let heroi ="Carlos"
+let xp = 444412321
+
+if(xp <= 1000){
+    nivel = "Ferro"
+    console.log("O Herói de nome " + heroi + " está no nível de " + nivel)
+}else if(1001 <= xp && xp <=2000){
+    nivel = "Bronze"
+    console.log("O Herói de nome " + heroi + " está no nível de " + nivel)
+}else if(2001 <= xp  && xp <= 5000){
+    nivel = "Prata"
+    console.log("O Herói de nome " + heroi + " está no nível de " + nivel)
+}else if(5001 <= xp && xp <= 7000){
+    nivel = "Ouro"
+    console.log("O Herói de nome " + heroi + " está no nível de " + nivel)
+}else if(7001 <= xp && xp <= 8000){
+    nivel = "Platina"
+    console.log("O Herói de nome " + heroi + " está no nível de " + nivel)
+}else if(8001 <= xp && xp <=9000){
+    nivel = "Ascendente"
+    console.log("O Herói de nome " + heroi + " está no nível de " + nivel)
+}else if(9001 <= xp && xp <= 10000){
+    nivel = "Imortal"
+    console.log("O Herói de nome " + heroi + " está no nível de " + nivel)
+}else if(xp >= 10001){
+    nivel = "Radiante"
+    console.log("O Herói de nome " + heroi + " está no nível de " + nivel)
+}
